a543f0a17af0e7438c173b9d02d5f8fc0e818d0f
NamikazeZero <87231196+NamikazeZero@users.noreply.github.com>
2021-11-18 16:38:09 +0700

    Add files via upload

---
+++ modules/Msf.sh
@@ -0,0 +1,9 @@
+apt install ruby
+apt install git
+pip install lolcat
+cd Metasploit
+git clone https://github.com/Hax4us/Metasploit_termux
+cd Metasploit_termux
+chmod +x metasploit.sh
+bash metasploit.sh
+exit
